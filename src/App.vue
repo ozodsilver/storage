24bab51a0c7fe277323cca9598570f85fd6947b4
ozodsilver <ozodsilver@gmail.com>
2022-02-02 00:37:16 +0500

    nav-changed

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="app">
   <Navigation> </Navigation>
-  
+  <Home> </Home>
   </div>
 </template>
 
 <script>
 import nav from './components/nav'
+import Home from './components/home'
 
 export default {
 components:{
-Navigation:nav
+Navigation:nav,
+Home
 },
 
   data(){
@@ -25,5 +27,7 @@ Navigation:nav
 *{
   margin: 0;
   padding: 0;
+  box-sizing: border-box;
+ font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 }
 </style>
---
+++ src/components/home.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <section>
+            <video src=".././assets/production ID_5155680.mp4" autoplay muted loop></video>
+        </section>
+    </div>
+</template>
+
+<script>
+export default {
+   
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+@import  './styles/home.scss'
+</style>
